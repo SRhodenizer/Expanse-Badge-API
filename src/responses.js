ce78ac37a1789c964de5b23a1e7ed7baba4dae75
SRhodenizer <scr3876@g.rit.edu>
2020-02-19 23:18:36 -0500

    fixed a small eslint error

--- src/responses.js
+++ src/responses.js
@@ -1,11 +1,31 @@
 // data object
 const users = [];
-//default preMade users to give examples of each of the frames 
-users[ "James Holden" ] = { badge: { frame: 'un', name: 'James Holden', sex: 'm', age: '30', image: '/media/jamesFace.jpg'} };
-users[ "Naomi Nagata" ] = { badge: { frame: 'opa', name: 'Naomi Nagata', sex: 'f', age: '29', image: '/media/naomiFace.jpg'} } ;
-users[ "Amos Burton" ] = { badge: { frame: 'protogen', name: 'Amos Burton', sex: 'm', age: '35', image: '/media/amosFace.jpg'} } ;
-users[ 'Alex Kamal' ] = { badge: { frame: 'mcrn', name: 'Alex Kamal', sex: 'm', age: '32', image: '/media/alexFace.jpg'} } ;
-users[ 'Josephus Miller' ] = { badge: { frame: 'sh', name: 'Josephus Miller', sex: 'm', age: '41', image: '/media/millerFace.jpg'} } ;
+// default preMade users to give examples of each of the frames
+users['James Holden'] = {
+  badge: {
+    frame: 'un', name: 'James Holden', sex: 'm', age: '30', image: '/media/jamesFace.jpg',
+  },
+};
+users['Naomi Nagata'] = {
+  badge: {
+    frame: 'opa', name: 'Naomi Nagata', sex: 'f', age: '29', image: '/media/naomiFace.jpg',
+  },
+};
+users['Amos Burton'] = {
+  badge: {
+    frame: 'protogen', name: 'Amos Burton', sex: 'm', age: '35', image: '/media/amosFace.jpg',
+  },
+};
+users['Alex Kamal'] = {
+  badge: {
+    frame: 'mcrn', name: 'Alex Kamal', sex: 'm', age: '32', image: '/media/alexFace.jpg',
+  },
+};
+users['Josephus Miller'] = {
+  badge: {
+    frame: 'sh', name: 'Josephus Miller', sex: 'm', age: '41', image: '/media/millerFace.jpg',
+  },
+};
 
 // responds with a json object
 const respondJSON = (request, response, status, object) => {
@@ -27,7 +47,7 @@ const respondJSONMeta = (request, response, status) => {
 // returns user as json objects
 const getUsers = (request, response, param) => {
   const obj = users[param.key];
-    console.log(param.key);
+  console.log(param.key);
 
   // makes a json object with the users data
   const responseJSON = {
