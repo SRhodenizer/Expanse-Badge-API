f42ed8f7fae04ccbc36ca99cc0bbcc91f751a39d
SRhodenizer <scr3876@g.rit.edu>
2020-02-19 23:17:17 -0500

    getting premade badges works, and posting a new badge to data works

--- src/responses.js
+++ src/responses.js
@@ -1,5 +1,11 @@
 // data object
-const users = { test: { badge: { frame: 'un', color: 'red' } } };
+const users = [];
+//default preMade users to give examples of each of the frames 
+users[ "James Holden" ] = { badge: { frame: 'un', name: 'James Holden', sex: 'm', age: '30', image: '/media/jamesFace.jpg'} };
+users[ "Naomi Nagata" ] = { badge: { frame: 'opa', name: 'Naomi Nagata', sex: 'f', age: '29', image: '/media/naomiFace.jpg'} } ;
+users[ "Amos Burton" ] = { badge: { frame: 'protogen', name: 'Amos Burton', sex: 'm', age: '35', image: '/media/amosFace.jpg'} } ;
+users[ 'Alex Kamal' ] = { badge: { frame: 'mcrn', name: 'Alex Kamal', sex: 'm', age: '32', image: '/media/alexFace.jpg'} } ;
+users[ 'Josephus Miller' ] = { badge: { frame: 'sh', name: 'Josephus Miller', sex: 'm', age: '41', image: '/media/millerFace.jpg'} } ;
 
 // responds with a json object
 const respondJSON = (request, response, status, object) => {
@@ -19,8 +25,9 @@ const respondJSONMeta = (request, response, status) => {
 };
 
 // returns user as json objects
-const getUsers = (request, response, key) => {
-  const obj = users.test;
+const getUsers = (request, response, param) => {
+  const obj = users[param.key];
+    console.log(param.key);
 
   // makes a json object with the users data
   const responseJSON = {
@@ -30,20 +37,13 @@ const getUsers = (request, response, key) => {
   respondJSON(request, response, 200, responseJSON);
 };
 
-// returns user as json objects
-const getUsersMeta = (request, response) => {
-  // responds with it
-  respondJSONMeta(request, response, 200);
-};
-
 // adds a user to the users data
 const addBadge = (request, response, body) => {
   const responseJSON = {
     message: 'All parameters are required.',
   };
-
   // if the needed params are not send tell the user
-  if (!body.name || !body.age) {
+  if (!body.name || !body.age || !body.sex || !body.faction) {
     responseJSON.id = 'missingParams';
     respondJSON(request, response, 400, responseJSON);
     return;
@@ -62,6 +62,8 @@ const addBadge = (request, response, body) => {
   // update the new objects fields
   users[body.name].name = body.name;
   users[body.name].age = body.age;
+  users[body.name].sex = body.sex;
+  users[body.name].faction = body.faction;
 
   // if the response code is still created return that message
   if (responseCode === 201) {
@@ -80,15 +82,9 @@ const getNotFound = (request, response) => {
   });
 };
 
-const getNotFoundMeta = (request, response) => {
-  respondJSONMeta(request, response, 404);
-};
-
 // export methods
 module.exports = {
   getUsers,
   addBadge,
   getNotFound,
-  getUsersMeta,
-  getNotFoundMeta,
 };
--- src/server.js
+++ src/server.js
@@ -13,109 +13,96 @@ const mediaHandler = require('./mediaResponses.js');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 // handle GET requests
-const handleGet = (request, response, parsedUrl, method) => {
-  // route to correct method based on url
-
+const handleGet = (request, response, parsedUrl, queryParam) => {
     //link to style sheet
-  if (parsedUrl.pathname === '/style.css') {
-    htmlHandler.getCSS(request, response);
-  } else
-      //link to index page
-  if (parsedUrl.pathname === '/') {
-    htmlHandler.getIndex(request, response);
-  } else
-      //gets a character id from the data object based on a name key
-  if (parsedUrl.pathname === '/getUsers') {
-      // make the response accessible
-      const res = response;
-
-      // make a body array
-      const body = [];
-
-      // errors on bad request
-      request.on('error', (err) => {
-        console.dir(err);
-        res.statusCode = 400;
-        res.end();
-      });
-
-      // adds data bytes to body array
-      request.on('data', (chunk) => {
-        body.push(chunk);
-      });
-
-      // when the upload stream ends
-      request.on('end', () => {
-        // take all the data in the byte array and makes it a string
-        const bodyString = Buffer.concat(body).toString();
-        // parses the string into objects by field name
-        const bodyParams = query.parse(bodyString);
-        // passes to addUser function
-
-        jsonHandler.getUsers(request, response, bodyParams);
-      });
+    if (parsedUrl.pathname === '/style.css') {
+        htmlHandler.getCSS(request, response);
+    } else
+        //link to index page
+        if (parsedUrl.pathname === '/') {
+            htmlHandler.getIndex(request, response);
+        } else
+            //gets a character id from the data object based on a name key
+            if (parsedUrl.pathname === '/getUsers') {
+                console.log(queryParam);
+                jsonHandler.getUsers(request,response, queryParam);
+            }
+    //cases for getting all the images from the media folder 
+    else if (parsedUrl.pathname === '/media/mcrn.png') {
+        mediaHandler.getMcrn(request, response);
+    } else if (parsedUrl.pathname === '/media/un.png') {
+        mediaHandler.getUn(request, response);
+    } else if (parsedUrl.pathname === '/media/sh.png') {
+        mediaHandler.getSh(request, response);
+    } else if (parsedUrl.pathname === '/media/opa.png') {
+        mediaHandler.getOpa(request, response);
+    } else if (parsedUrl.pathname === '/media/protogen.png') {
+        mediaHandler.getProto(request, response);
+    }else if (parsedUrl.pathname === '/media/jamesFace.jpg') {
+        mediaHandler.getJamesFace(request, response);
+    } else if (parsedUrl.pathname === '/media/naomiFace.jpg') {
+        mediaHandler.getNaomiFace(request, response);
+    } else if (parsedUrl.pathname === '/media/amosFace.jpg') {
+        mediaHandler.getAmosFace(request, response);
+    } else if (parsedUrl.pathname === '/media/alexFace.jpg') {
+        mediaHandler.getAlexFace(request, response);
+    } else if (parsedUrl.pathname === '/media/millerFace.jpg') {
+        mediaHandler.getMillerFace(request, response);
+        //404 default case 
+    } else {
+        jsonHandler.getNotFound(request, response);
     }
-   else if (parsedUrl.pathname === '/media/mcrn.png') {
-      mediaHandler.getMcrn(request, response);
-   }else if (parsedUrl.pathname === '/media/un.png') {
-      mediaHandler.getUn(request, response);
-   }else if (parsedUrl.pathname === '/media/sh.png') {
-      mediaHandler.getSh(request, response);
-   }else if (parsedUrl.pathname === '/media/opa.png') {
-      mediaHandler.getOpa(request, response);
-   }else if (parsedUrl.pathname === '/media/protogen.png') {
-      mediaHandler.getProto(request, response);
-      //404 default case 
-  } else {
-    jsonHandler.getNotFound(request, response);
-  }
 };
 
 const handlePost = (request, response, parsedUrl) => {
-  // if the post call is addUser
-  if (parsedUrl.path === '/addBadge') {
-    // make the response accessible
-    const res = response;
+    // if the post call is addUser
+    if (parsedUrl.path === '/addBadge') {
+        // make the response accessible
+        const res = response;
 
-    // make a body array
-    const body = [];
+        // make a body array
+        const body = [];
 
-    // errors on bad request
-    request.on('error', (err) => {
-      console.dir(err);
-      res.statusCode = 400;
-      res.end();
-    });
+        // errors on bad request
+        request.on('error', (err) => {
+            console.dir(err);
+            res.statusCode = 400;
+            res.end();
+        });
 
-    // adds data bytes to body array
-    request.on('data', (chunk) => {
-      body.push(chunk);
-    });
+        // adds data bytes to body array
+        request.on('data', (chunk) => {
+            body.push(chunk);
+        });
 
-    // when the upload stream ends
-    request.on('end', () => {
-      // take all the data in the byte array and makes it a string
-      const bodyString = Buffer.concat(body).toString();
-      // parses the string into objects by field name
-      const bodyParams = query.parse(bodyString);
-      // passes to addUser function
-      jsonHandler.addBadge(request, res, bodyParams);
-    });
-  }
+        // when the upload stream ends
+        request.on('end', () => {
+            // take all the data in the byte array and makes it a string
+            const bodyString = Buffer.concat(body).toString();
+            // parses the string into objects by field name
+            const bodyParams = query.parse(bodyString);
+            // passes to addUser function
+            console.log(bodyParams);
+            jsonHandler.addBadge(request, res, bodyParams);
+        });
+    }
 };
 
 const onRequest = (request, response) => {
-  // parse the url using the url module
-  // This will let us grab any section of the URL by name
-  const parsedUrl = url.parse(request.url);
+    // parse the url using the url module
+    // This will let us grab any section of the URL by name
+    const parsedUrl = url.parse(request.url);
 
-  if (request.method === 'POST') {
-    // handles the post request
-    handlePost(request, response, parsedUrl);
-  } else {
-    // handles the get request
-    handleGet(request, response, parsedUrl, request.method);
-  }
+    // gets the query information from the parsed URL
+    const params = query.parse(parsedUrl.query);
+
+    if (request.method === 'POST') {
+        // handles the post request
+        handlePost(request, response, parsedUrl);
+    } else {
+        // handles the get request
+        handleGet(request, response, parsedUrl, params);
+    }
 };
 
 http.createServer(onRequest).listen(port);
